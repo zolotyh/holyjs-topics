5471beb36b7a41cfb197c9ac67771063179c8c9c
Aleksei Zolotykh <aleksei.zolotykh@myoffice.team>
2023-01-23 06:01:52 +0300

    minor changes

--- .eleventy.js
+++ .eleventy.js
@@ -3,19 +3,94 @@ const cleanCSS = require("clean-css");
 const { cache } = require("eleventy-plugin-workbox");
 const { compress } = require("eleventy-plugin-compress");
 
+const markdownIt = require("markdown-it");
+const markdownItAnchor = require("markdown-it-anchor");
+
+const pluginRss = require("@11ty/eleventy-plugin-rss");
+const pluginSyntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
+const pluginNavigation = require("@11ty/eleventy-navigation");
+
+const { DateTime } = require("luxon");
+
 module.exports = function (eleventyConfig) {
+  eleventyConfig.addPassthroughCopy({
+    "./node_modules/alpinejs/dist/cdn.min.js": "./assets/js/alpine.js",
+    "./node_modules/prismjs/themes/prism-okaidia.min.css":
+      "./assets/css/prism-okaidia.min.css",
+    "./src/assets/fonts/*": "./assets/fonts",
+    "./src/assets/img/posts/*": "./assets/img/posts",
+    "./src/assets/img/*": "./assets/img",
+  });
+
+  // https://html.spec.whatwg.org/multipage/common-microsyntaxes.html#valid-date-string
+  eleventyConfig.addFilter("htmlDateString", (dateObj) => {
+    return DateTime.fromJSDate(dateObj, { zone: "utc" }).toFormat("yyyy-LL-dd");
+  });
+
+  eleventyConfig.addFilter("readableDate", (dateObj) => {
+    return DateTime.fromJSDate(dateObj, { zone: "utc" }).toFormat(
+      "dd LLL yyyy"
+    );
+  });
+
   eleventyConfig.addFilter("cssmin", function (code) {
     return new cleanCSS({}).minify(code).styles;
   });
 
-  eleventyConfig.addPassthroughCopy({
-    "./node_modules/alpinejs/dist/cdn.min.js": "./assets/js/alpine.js",
-    "./src/assets/fonts/*": "./assets/fonts",
-    "./src/assets/img/*": "./assets/img",
-  });
-
   eleventyConfig.addPlugin(compress, { enabled: true });
   eleventyConfig.addPlugin(cache);
+  eleventyConfig.addPlugin(pluginRss);
+  eleventyConfig.addPlugin(pluginSyntaxHighlight);
+  eleventyConfig.addPlugin(pluginNavigation);
+
+  // Get the first `n` elements of a collection.
+  eleventyConfig.addFilter("head", (array, n) => {
+    if (!Array.isArray(array) || array.length === 0) {
+      return [];
+    }
+    if (n < 0) {
+      return array.slice(n);
+    }
+
+    return array.slice(0, n);
+  });
+
+  // Return the smallest number argument
+  eleventyConfig.addFilter("min", (...numbers) => {
+    return Math.min.apply(null, numbers);
+  });
+
+  function filterTagList(tags) {
+    return (tags || []).filter(
+      (tag) => ["all", "nav", "post", "posts"].indexOf(tag) === -1
+    );
+  }
+
+  eleventyConfig.addFilter("filterTagList", filterTagList);
+
+  // Create an array of all tags
+  eleventyConfig.addCollection("tagList", function (collection) {
+    let tagSet = new Set();
+    collection.getAll().forEach((item) => {
+      (item.data.tags || []).forEach((tag) => tagSet.add(tag));
+    });
+
+    return filterTagList([...tagSet]);
+  });
+
+  const markdownLibrary = markdownIt({
+    html: true,
+    linkify: true,
+  }).use(markdownItAnchor, {
+    permalink: markdownItAnchor.permalink.ariaHidden({
+      placement: "after",
+      class: "direct-link",
+      symbol: "#",
+    }),
+    level: [1, 2, 3, 4],
+    slugify: eleventyConfig.getFilter("slugify"),
+  });
+  eleventyConfig.setLibrary("md", markdownLibrary);
 
   eleventyConfig.addTransform("htmlmin", (content, outputPath) => {
     if (outputPath.endsWith(".html")) {
@@ -30,9 +105,15 @@ module.exports = function (eleventyConfig) {
   });
 
   return {
+    templateFormats: ["md", "njk", "html", "liquid"],
+    pathPrefix: "/",
+    markdownTemplateEngine: "njk",
+    htmlTemplateEngine: "njk",
+
     dir: {
       input: "src",
       output: "docs",
+      data: "_data",
       includes: "_includes",
       layouts: "_layouts",
     },
